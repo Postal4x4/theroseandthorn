5279aac0caa9a5af8c6a212fddc33b4604cad93a
Postal4x4 <aaron.fudge@gmail.com>
2026-07-03 18:03:03 -0400

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,12 @@
 function togglePersona(el) {
   el.classList.toggle("active");
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".persona-card");
+
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("active");
+    });
+  });
+});
